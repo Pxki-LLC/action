6319da65320dbc8718792869db54c8d3a4f228db
Jinkkuu <reddabomb82@gmail.com>
2024-06-01 15:33:10 -0400

    Fix launcher not using compiled bootstrap

--- launcher.py
+++ launcher.py
@@ -72,7 +72,7 @@ for a in os.listdir(resource_path(modulepath)):
         exit()
         os.remove(resource_path(modulepath+a))
 moduletime=time.time()-moduletime
-if os.path.isfile(modulepath+'bootstrap.py'):
+if os.path.isfile(resource_path(modulepath+'bootstrap.py')):
         exec(open(resource_path(modulepath+'bootstrap.py')).read())
 else:
     print('Bootstrap not Found')
